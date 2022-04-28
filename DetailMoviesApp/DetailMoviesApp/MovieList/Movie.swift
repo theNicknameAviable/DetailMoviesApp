d24ae267583f52b42a5e2b5053fc31e48e308a5b
Raul Bautista <rbmolina27@gmail.com>
2022-04-28 20:42:22 +0200

    New ViewModel, ViewControler, Model (MovieList)

---
+++ DetailMoviesApp/DetailMoviesApp/MovieList/Movie.swift
@@ -0,0 +1,8 @@
+//
+//  Movie.swift
+//  DetailMoviesApp
+//
+//  Created by Raul Bautista on 28/4/22.
+//
+
+import Foundation
---
+++ DetailMoviesApp/DetailMoviesApp/MovieList/MovieListVM.swift
@@ -0,0 +1,8 @@
+//
+//  MovieListVM.swift
+//  DetailMoviesApp
+//
+//  Created by Raul Bautista on 28/4/22.
+//
+
+import Foundation
