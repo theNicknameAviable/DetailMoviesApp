1c130fc85607be9ecc76fcb7228119b608287b6f
Raul Bautista <rbmolina27@gmail.com>
2022-04-28 20:23:24 +0200

    initial commit

---
+++ DetailMoviesApp/DetailMoviesApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DetailMoviesApp
+//
+//  Created by Raul Bautista on 28/4/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
