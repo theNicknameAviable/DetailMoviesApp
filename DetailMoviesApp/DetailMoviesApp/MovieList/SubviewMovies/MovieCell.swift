a4c88e67085d2567361a5b0f4bf1d965ca036a4b
Raul Bautista <rbmolina27@gmail.com>
2022-04-29 17:51:30 +0200

    configure custom cell

--- DetailMoviesApp/DetailMoviesApp/MovieList/SubviewMovies/MovieCell.swift
+++ DetailMoviesApp/DetailMoviesApp/MovieList/SubviewMovies/MovieCell.swift
@@ -5,4 +5,21 @@
 //  Created by Raul Bautista on 29/4/22.
 //
 
-import Foundation
+import UIKit
+
+class MovieCell: UITableViewCell {
+    
+    @IBOutlet weak var filmPoster: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var filmDescription: UILabel!
+    @IBOutlet weak var voteAverage: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        configureView()
+    }
+    
+    func configureView() {
+        backgroundColor = .white
+    }
+}
