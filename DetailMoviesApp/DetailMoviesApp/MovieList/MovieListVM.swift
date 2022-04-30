4f1d747db6e570b17a476faaa01834180500a5a9
Raul Bautista <rbmolina27@gmail.com>
2022-04-30 10:13:38 +0200

    init networker in ViewModelMovieList class

--- DetailMoviesApp/DetailMoviesApp/MovieList/MovieListVM.swift
+++ DetailMoviesApp/DetailMoviesApp/MovieList/MovieListVM.swift
@@ -10,4 +10,9 @@ import Foundation
 class ViewModelMovieList {
     
     var movieList: [Movies] = []
+    let networker: NetworkWorker
+    
+    init (networker: NetworkWorker = NetworkWorker()){
+        self.networker = networker
+    }
 }
